a757e075d173e73e1a83f08933d98635e3ff5f82
Daniel Bellido <103462533+DaniBellido@users.noreply.github.com>
2024-04-07 20:03:30 +0100

    Uploading Qt project

---
+++ build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_Notepad_FILE D:/Development/My C++/Qt Projects/Notepad/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/Notepad.exe)
+set(__QT_DEPLOY_TARGET_Notepad_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
